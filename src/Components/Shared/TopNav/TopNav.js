6c29c0af1067f354f22cfeb8644e4450f9c3629f
Sakib Noman <skbnmn@gmail.com>
2021-05-03 00:31:18 +0600

    nav hover

--- src/Components/Shared/TopNav/TopNav.js
+++ src/Components/Shared/TopNav/TopNav.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Button, Nav, Navbar } from 'react-bootstrap';
 import { Link, useParams } from 'react-router-dom';
+import './TopNav.css';
 
 const TopNav = () => {
 
     const { isContact } = useParams()
-    console.log(isContact);
 
     return (
         <Navbar bg="transparent" expand="lg">
@@ -13,11 +13,11 @@ const TopNav = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
-                    <Nav.Link as={Link} className={`ml-4 ${isContact ? "text-white" : "text-dark"}`} to="/home">Home</Nav.Link>
-                    <Nav.Link as={Link} className={`ml-4 ${isContact ? "text-white" : "text-dark"}`} to="/about">About</Nav.Link>
-                    <Nav.Link as={Link} className={`ml-4 ${isContact ? "text-white" : "text-dark"}`} to="/portfolio">Portfolios</Nav.Link>
-                    <Nav.Link as={Link} className={`ml-4 ${isContact ? "text-white" : "text-dark"}`} to="/blogs">Blogs</Nav.Link>
-                    <Nav.Link as={Link} className={`ml-4 ${isContact ? "text-white" : "text-dark"}`} to="/contact/true">Contact</Nav.Link>
+                    <Nav.Link as={Link} className={`ml-4 ${isContact ? "navvy2 text-white" : "navvy text-dark"}`} to="/home">Home</Nav.Link>
+                    <Nav.Link as={Link} className={`ml-4 ${isContact ? "navvy2 text-white" : "navvy text-dark"}`} to="/about">About</Nav.Link>
+                    <Nav.Link as={Link} className={`ml-4 ${isContact ? "navvy2 text-white" : "navvy text-dark"}`} to="/portfolio">Portfolios</Nav.Link>
+                    <Nav.Link as={Link} className={`ml-4 ${isContact ? "navvy2 text-white" : "navvy text-dark"}`} to="/blogs">Blogs</Nav.Link>
+                    <Nav.Link as={Link} className={`ml-4 ${isContact ? "navvy2 text-white" : "navvy text-dark"}`} to="/contact/true">Contact</Nav.Link>
                 </Nav>
                 <a href="https://drive.google.com/uc?export=download&id=1bvWFgbl9FN7ooDEqlYrWmh1yY22XyoYq">
                     <Button variant="outline-success" className="ml-4" >Resume</Button>
