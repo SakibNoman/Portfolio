d40d8a3fdbf1598628c0e8ad0e2f9999aa8efc50
Sakib Noman <skbnmn@gmail.com>
2021-05-07 06:49:55 +0600

    projects updated

--- src/Components/Home/Portfolios/Portfolios.js
+++ src/Components/Home/Portfolios/Portfolios.js
@@ -1,17 +1,29 @@
 import QueueAnim from 'rc-queue-anim';
 import React from 'react';
 import { Container } from 'react-bootstrap';
-import callingImg from '../../../images/Calling-Cabs.png';
-import gentsImg from '../../../images/Gents-Glamor.png';
-import refreshImg from '../../../images/Refresh-Professional-Painting-Service.png';
+import callingImg1 from '../../../images/Calling-Cabs.png';
+import callingImg2 from '../../../images/Calling-Cabs2.png';
+import callingImg3 from '../../../images/Calling-Cabs3.png';
+import callingImg4 from '../../../images/Calling-Cabs4.png';
+import gentsImg1 from '../../../images/Gents-Glamor.png';
+import gentsImg2 from '../../../images/Gents-Glamor2.png';
+import gentsImg3 from '../../../images/Gents-Glamor3.png';
+import gentsImg4 from '../../../images/Gents-Glamor4.png';
+import refreshImg1 from '../../../images/Refresh-Professional-Painting-Service.png';
+import refreshImg2 from '../../../images/Refresh-Professional-Painting-Service2.png';
+import refreshImg3 from '../../../images/Refresh-Professional-Painting-Service3.png';
+import refreshImg4 from '../../../images/Refresh-Professional-Painting-Service4.png';
 import WorkCard from './WorkCard';
 
 const works = [
     {
         id: 1,
         name: 'Refresh Painting',
-        image: refreshImg,
-        desc: "User can take different services, and track the state of services, pay online.",
+        image1: refreshImg1,
+        image2: refreshImg2,
+        image3: refreshImg3,
+        image4: refreshImg4,
+        desc: "User can take different services as their need, track status of their taken services, see all order history, pay online through credit card. Admin can add new service and delete service that no longer available. Admin can view all order of services and change status of services.Admin can add new admin",
         tools: ["React.js", "Node.js", "Express.js", "MongoDB", "Firebase", "Stripe.js"],
         repo: 'https://github.com/SakibNoman/Refresh-Painting-Client',
         live: 'https://refresh-painting.web.app/'
@@ -19,8 +31,11 @@ const works = [
     {
         id: 2,
         name: 'Gents Glamor',
-        image: gentsImg,
-        desc: '1. User can take different services, and track the state of services, pay online.2. Admin can add/delete services, change the state of services.3. User can add a review, and these are shown on the home page.',
+        image1: gentsImg1,
+        image2: gentsImg2,
+        image3: gentsImg3,
+        image4: gentsImg4,
+        desc: 'Calling Cabs is a react app where you can find various types of transport system. We provide services of Family tour, Emergency transport, Goods transportation on reasonable price. This app is easy to use. Just select your desire vehicle and choose your pick up point and destination you will see it on map also.Thank you!',
         tools: ["React.js", "Node.js", "Express.js", "MongoDB", "Firebase"],
         repo: 'https://github.com/SakibNoman/Gents-Glamor-Client',
         live: 'https://gents-glamor.web.app/'
@@ -28,8 +43,11 @@ const works = [
     {
         id: 3,
         name: 'Calling Cabs',
-        image: callingImg,
-        desc: 'User can rent different transport as their need, search place.Firebase Authentication',
+        image1: callingImg1,
+        image2: callingImg2,
+        image3: callingImg3,
+        image4: callingImg4,
+        desc: 'User can buy products,see their ordered products. Admin can add new product and,delete existing product. Admin can see all ordered products. User have to login to buy products. User specific data will save in database. Currently Admin panel is open for all user.',
         tools: ["React", "React Router", "Firebase"],
         repo: 'https://github.com/SakibNoman/Calling-Cabs/',
         live: 'https://calling-cabss.web.app/'
--- src/Components/Home/Portfolios/WorkCard.js
+++ src/Components/Home/Portfolios/WorkCard.js
@@ -4,15 +4,15 @@ import { Badge, Card, ListGroup, ListGroupItem } from 'react-bootstrap';
 import { GoLinkExternal, GoMarkGithub } from "react-icons/go";
 
 const WorkCard = ({ info, key }) => {
-    const { name, image, desc, tools, repo, live, id } = info
+    const { name, image1, image2, image3, image4, desc, tools, repo, live, id } = info
     const slides = [
-        <img src={image} alt="1" />, <img src={image} alt="2" />, <img src={image} alt="3" />
+        <img src={image1} alt="1" />, <img src={image2} alt="2" />, <img src={image3} alt="3" />, <img src={image4} alt="3" />
     ]
     return (
         <div className="col-md-12 px-md-0 mx-3">
             <Card key={id} className="my-3 shadow row flex-row">
-                <div className="col-md-6 mt-3" >
-                    <Carousel slides={slides} autoplay={true} interval={4000} />
+                <div className="col-md-6 mt-5 pt-md-4" >
+                    <Carousel className="" slides={slides} autoplay={true} interval={4000} />
                 </div>
                 <div className="col-md-6" >
                     <Card.Body className="mt-0" >
@@ -22,7 +22,7 @@ const WorkCard = ({ info, key }) => {
                         </Card.Text>
                     </Card.Body>
                     <ListGroup className="list-group-flush">
-                        <ListGroupItem>{tools.map(each => <h5 className="d-inline mr-2" ><Badge variant="dark" pill>{each}</Badge></h5>)}</ListGroupItem>
+                        <ListGroupItem>{tools.map(each => <h5 className="d-inline mr-2" ><Badge variant="dark">{each}</Badge></h5>)}</ListGroupItem>
                     </ListGroup>
                     <Card.Body>
                         <Card.Link className="text-dark" target="_blank" href={repo}><GoMarkGithub fontSize="25px" /> <small>Code</small> </Card.Link>
