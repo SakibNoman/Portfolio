98cc304b73dc358fe9884dcae5bfb8a71755d9fd
Sakib Noman <skbnmn@gmail.com>
2021-09-08 22:25:36 +0600

    more project added

--- src/Components/Home/Portfolios/Portfolios.js
+++ src/Components/Home/Portfolios/Portfolios.js
@@ -1,6 +1,10 @@
 import React, { useEffect } from 'react';
 import { Container, Row } from 'react-bootstrap';
 import { Link, useLocation } from 'react-router-dom';
+import abImg1 from '../../../images/ab1.png';
+import abImg2 from '../../../images/ab2.png';
+import abImg3 from '../../../images/ab3.png';
+import abImg4 from '../../../images/ab4.png';
 import callingImg1 from '../../../images/Calling-Cabs.png';
 import callingImg2 from '../../../images/Calling-Cabs2.png';
 import callingImg3 from '../../../images/Calling-Cabs3.png';
@@ -66,6 +70,19 @@ const works = [
         repo: 'https://github.com/SakibNoman/Chittagong-Super-League',
         live: 'https://eloquent-curran-574b86.netlify.app/'
     }
+    ,
+    {
+        id: 5,
+        name: 'Athletics Buzz',
+        image1: abImg3,
+        image2: abImg2,
+        image3: abImg1,
+        image4: abImg4,
+        desc: 'Athletics Buzz is a react app where you can find useful information on various leagues of the world. Not only the information of a single sport but also all types of sports leagues information you can find in this app.',
+        tools: ["React", "React Router", "Netlify", "React-Bootstrap", "Font Awesome"],
+        repo: 'https://github.com/SakibNoman/Athletics-Buzz',
+        live: 'https://60490451f6a15f07d7550c29--stupefied-mestorf-a36e41.netlify.app/league/4339/Turkish%20Super%20Lig'
+    }
 ]
 
 const Portfolios = () => {
@@ -81,7 +98,10 @@ const Portfolios = () => {
             <h2 className="text-center" >Portfolios</h2>
             <div type="scale" className="row justify-content-center my-5 mx-0" >
                 {
-                    works.map(each => <WorkCard key={each.id} info={each} ></WorkCard>)
+                    works.map((each, index) => {
+                        if (index >= 3 && !isPortfolio) return "";
+                        else return <WorkCard key={each.id} info={each} ></WorkCard>
+                    })
                 }
             </div>
             <div className="mb-5 text-center "><Row as={Link} to="/portfolio" className={`text-decoration-none  ${isPortfolio ? "d-none" : "d-block"}`} ><button className="btn btn-outline-success">See more projects</button></Row></div>
